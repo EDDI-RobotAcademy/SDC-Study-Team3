9c7cffa8630932389a45dc78d835e11dc85a3500
bikjo99 <149447229+bikjo99@users.noreply.github.com>
2023-11-20 17:51:30 +0900

    feat: [이성용] 11월20일 작업물 [TEAM3-x]

---
+++ sungyonglee1/board/adapter/api/list_handler/list_read_data/list_read_data_handler.h
@@ -0,0 +1,18 @@
+#ifndef __LIST_READ_DATA_HANDLER_H__
+#define __LIST_READ_DATA_HANDLER_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdbool.h>
+
+void read_content_from_file_and_print(int, char *);
+//void read_content_from_file(int, char *);
+void reset_file_pointer(int);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
